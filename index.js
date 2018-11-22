577b50d8a394527c01c507032c2305103412eaba
Robin Millette <robin@millette.info>
2018-11-22 01:28:16 -0500

    Method to check database on launch.

--- index.js
+++ index.js
@@ -8,33 +8,26 @@ const bodyParser = require("koa-bodyparser")
 const { Pool } = require("pg").native
 
 // self
-const { login } = require("./lib")
+const { login, front } = require("./lib")
 
-const visitorPool = new Pool({
+const app = new Koa()
+
+const pool = new Pool({
   user: process.env.SITEVISITOR,
   password: process.env.SITEVISITORPW,
   connectionString: process.env.SITECONN,
 })
 
-const front = async (ctx) => {
-  if (ctx.path !== "/") {
-    ctx.status = 404
-    return
-  }
-  const n = Date.now()
-  const res = await visitorPool.query("SELECT * from table1")
-  ctx.type = "text/html"
-  ctx.body = `
-    ${JSON.stringify(res.rows)} -
-    ${Date.now() - n};
-    <a href='/login'>login</a>
-  `
-}
+const checkPool = () =>
+  pool
+    .connect()
+    .then((client) => client.release())
+    .then(() => {
+      app.context.visitorPool = pool
+      app.use(bodyParser())
+      app.use(mount("/login", login))
+      app.use(mount(front))
+      return app
+    })
 
-const app = new Koa()
-
-app.use(bodyParser())
-app.use(mount("/login", login))
-app.use(mount("/", front))
-
-module.exports = app
+module.exports = checkPool
